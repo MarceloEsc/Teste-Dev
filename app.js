634bd259aabc01c5ccf3723a534202fde8a8a85e
MarceloEsc <marceloescobarjunior@gmail.com>
2020-07-08 13:24:44 -0300

    ajustes

--- app.js
+++ app.js
@@ -14,17 +14,100 @@ const getDeviceType = () => {
     return 'desktop';
 };
 
+// inicia as ações do layout mobile
+const initMobile = () => {
+    const colors = document.querySelector('.colors');
+    colors.onclick = event => {
+        if (event.target.classList.contains('checkbox') && !event.target.classList.contains('checked')) {
+            selectFilter('.colors div .checkbox', event.target);
+            updateClothsIntoDOM(event.target.id);
+        }
+    }
+    const sizes = document.querySelector('.sizes');
+    sizes.onclick = event => {
+        if (event.target.tagName === 'DIV') {
+            selectFilter('.sizes div', event.target);
+            updateClothsIntoDOM(event.target.id);
+        }
+    }
+    const prices = document.querySelector('.prices');
+    prices.onclick = event => {
+        const low = event.target.dataset.lowprice;
+        const big = event.target.dataset.bigprice;
+
+        if (event.target.classList.contains('checkbox')) {
+            selectFilter('.prices div .checkbox', event.target);
+            updateClothsIntoDOM(low, big);
+        }
+    }
+    const expandColor = document.querySelector('#expand_colors');
+    let isColorExpanded = false;
+    expandColor.onclick = () => {
+        if (!isColorExpanded) {
+            document.querySelector('.colors').style.height = 'unset';
+            isColorExpanded = true;
+        } else {
+            document.querySelector('.colors').style.height = '30px';
+            isColorExpanded = false;
+        }
+    }
+
+    const expandSize = document.querySelector('#expand_sizes');
+    let isSizeExpanded = false;
+    expandSize.onclick = () => {
+        if (!isSizeExpanded) {
+            document.querySelector('.sizes').style.height = 'unset';
+            isSizeExpanded = true
+        } else {
+            document.querySelector('.sizes').style.height = '30px';
+            isSizeExpanded = false;
+        }
+    }
+
+    const expandPrice = document.querySelector('#expand_prices');
+    let isPriceExpanded = false;
+    expandPrice.onclick = () => {
+        if (!isPriceExpanded) {
+            document.querySelector('.prices').style.height = 'unset';
+            isPriceExpanded = true;
+        } else {
+            document.querySelector('.prices').style.height = '30px';
+            isPriceExpanded = false;
+        }
+    }
+
+    const closeFilter = document.querySelector('#close_filters');
+    closeFilter.onclick = () => {
+        document.querySelector('.mobile-filter').style.display = 'none';
+    }
+
+    const openFilter = document.querySelector('#filter_btn');
+    openFilter.onclick = () => {
+        document.querySelector('.mobile-filter').style.display = 'block';
+    }
+
+    const closeOrder = document.querySelector('#close_orders');
+    closeOrder.onclick = () => {
+        document.querySelector('.mobile-order').style.display = 'none';
+    }
+
+    const openOrder = document.querySelector('#order_btn');
+    openOrder.onclick = () => {
+        document.querySelector('.mobile-order').style.display = 'block';
+    }
+}
+
 // insere a estilização móvel se o dispositivo for móvel
 const insertMobileStyle = () => {
     const device = getDeviceType();
     console.log(device);
-
 
     if (device == 'tablet' || device == 'mobile') {
         const link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = './mobile-style.css';
         document.querySelector('head').append(link);
+        initMobile();
     }
 }
 insertMobileStyle();
@@ -53,8 +136,8 @@ let clothesArr,
 console.log(clothesCartArr);
 
 if (clothesCartArr.length !== 0) {
-    cartNumberIcon.classList.remove('hidden')
-    cartNumberIcon.innerHTML = clothesCartArr.length
+    cartNumberIcon.classList.remove('hidden');
+    cartNumberIcon.innerHTML = clothesCartArr.length;
 }
 
 // insere os produtos na pagina
@@ -62,8 +145,8 @@ const insertClothsIntoDOM = async () => {
     clothes = await getClothes();
 
     clothes.map(({ id, name, price, maxTimes, timesValue, image }) => {
-        let template = document.createElement('div')
-        template.classList.add('clothes')
+        let template = document.createElement('div');
+        template.classList.add('clothes');
         template.innerHTML = `
                 <img src="./images/${image}" alt="${name}" name="${name}">
                 <div class="info">
@@ -74,14 +157,15 @@ const insertClothsIntoDOM = async () => {
                 </div>
                 <button onclick="addToCart(${id})">COMPRAR</button>
             </div>`
-        clothesDisplay.append(template)
+        clothesDisplay.append(template);
     })
     console.log(clothes);
 }
-insertClothsIntoDOM()
+insertClothsIntoDOM();
 
+// atualiza podutos a partir da filtragem
 const updateClothsIntoDOM = (param, maxparam = null) => {
-    clothesDisplay.innerHTML = ''
+    clothesDisplay.innerHTML = '';
     clothes.map(({ id, name, price, sizes, colors, maxTimes, timesValue, image }) => {
         if (colors === param
             || price >= param && price <= maxparam
@@ -98,7 +182,7 @@ const updateClothsIntoDOM = (param, maxparam = null) => {
                     <button onclick="addToCart(${id})">COMPRAR</button>
                 </div>`
 
-            clothesDisplay.innerHTML += template
+            clothesDisplay.innerHTML += template;
         }
     })
 }
@@ -111,21 +195,23 @@ const addToCart = (id) => {
     updateCartItems(clothesCartArr);
 }
 
+// remove produto do carrinho
 const removeFromCart = (id) => {
     clothesCartArr = clothesCartArr.filter(clothes => clothes.id !== id);
-    sessionStorage.setItem('cart', JSON.stringify(clothesCartArr))
-    updateCartItems(clothesCartArr)
+    sessionStorage.setItem('cart', JSON.stringify(clothesCartArr));
+    updateCartItems(clothesCartArr);
 }
 
+// atualiza produtos do carrinho
 const updateCartItems = (arr) => {
     const totalPrice = arr.reduce((totalPrice, { price }) => totalPrice + price, 0);
 
     if (arr.length > 0) {
-        cartNumberIcon.classList.remove('hidden')
-        cartNumberIcon.innerHTML = arr.length
+        cartNumberIcon.classList.remove('hidden');
+        cartNumberIcon.innerHTML = arr.length;
     }
 
-    let template = ''
+    let template = '';
     arr.map(clothes => {
 
         template += `<div><span>${clothes.name.toUpperCase()}</span>
@@ -134,123 +220,65 @@ const updateCartItems = (arr) => {
         </div>`
     })
 
-    const cartDisplay = document.querySelector('#products')
-    const finalPrice = document.querySelector('#final_price')
-    cartDisplay.innerHTML = template
-    finalPrice.innerHTML = totalPrice.toFixed(2)
+    const cartDisplay = document.querySelector('#products');
+    const finalPrice = document.querySelector('#final_price');
+    cartDisplay.innerHTML = template;
+    finalPrice.innerHTML = totalPrice.toFixed(2);
 }
-updateCartItems(clothesCartArr)
+updateCartItems(clothesCartArr);
 
+// interação com os botões de filtragem
 const selectFilter = (element, selected) => {
-    const DOMFilter = document.querySelectorAll(`${element}`)
+    const DOMFilter = document.querySelectorAll(`${element}`);
     DOMFilter.forEach(element => {
         if (element == selected && !selected.classList.contains('checked')) {
-            element.classList.add('checked')
+            element.classList.add('checked');
         }
         else {
-            element.classList.remove('checked')
+            element.classList.remove('checked');
         }
     })
 }
 
-const loadMore = document.querySelector('#load_more')
-loadMore.onclick = () => {
-    insertClothsIntoDOM()
-}
+const loadMore = document.querySelector('#load_more');
+loadMore.onclick = () => insertClothsIntoDOM();
 
-const colors = document.querySelector('.colors')
+const colors = document.querySelector('.filter .colors');
 colors.onclick = event => {
     if (event.target.classList.contains('checkbox') && !event.target.classList.contains('checked')) {
-        selectFilter('.colors div .checkbox', event.target)
-        updateClothsIntoDOM(event.target.id)
+        selectFilter('.colors div .checkbox', event.target);
+        updateClothsIntoDOM(event.target.id);
     }
 }
-const sizes = document.querySelector('.sizes')
+const sizes = document.querySelector('.filter .sizes');
 sizes.onclick = event => {
     if (event.target.tagName === 'DIV') {
-        selectFilter('.sizes div', event.target)
-        updateClothsIntoDOM(event.target.id)
+        selectFilter('.sizes div', event.target);
+        updateClothsIntoDOM(event.target.id);
     }
 }
-const prices = document.querySelector('.prices')
+const prices = document.querySelector('.filter .prices');
 prices.onclick = event => {
-    const low = event.target.dataset.lowprice
-    const big = event.target.dataset.bigprice
+    const low = event.target.dataset.lowprice;
+    const big = event.target.dataset.bigprice;
 
     if (event.target.classList.contains('checkbox')) {
-        selectFilter('.prices div .checkbox', event.target)
-        updateClothsIntoDOM(low, big)
+        selectFilter('.prices div .checkbox', event.target);
+        updateClothsIntoDOM(low, big);
     }
 }
 
-const modal = document.querySelector('#cart')
-const closeModal = () => {
-    modal.classList.toggle('hidden')
-}
+const modal = document.querySelector('#cart');
+const closeModal = () => modal.classList.toggle('hidden');
 
-const cart = document.querySelector('.nav i')
-cart.onclick = () => {
-    modal.classList.toggle('hidden')
-}
+const cart = document.querySelector('.nav i');
+cart.onclick = () => modal.classList.toggle('hidden');
 
-const moreColors = document.querySelector('#more_colors')
+const moreColors = document.querySelector('#more_colors');
 moreColors.onclick = () => {
-    document.querySelector('.colors').style.height = 'unset'
-    moreColors.style.display = 'none'
+    document.querySelector('.filter .colors').style.height = 'unset';
+    moreColors.style.display = 'none';
 }
 
-const expandColor = document.querySelector('#expand_colors')
-let isColorExpanded = false
-expandColor.onclick = () => {
-    if (!isColorExpanded) {
-        document.querySelector('.colors').style.height = 'unset'
-        isColorExpanded = true
-    } else {
-        document.querySelector('.colors').style.height = '30px'
-        isColorExpanded = false
-    }
-}
-
-const expandSize = document.querySelector('#expand_sizes')
-let isSizeExpanded = false
-expandSize.onclick = () => {
-    if (!isSizeExpanded) {
-        document.querySelector('.sizes').style.height = 'unset'
-        isSizeExpanded = true
-    } else {
-        document.querySelector('.sizes').style.height = '30px'
-        isSizeExpanded = false
-    }
-}
-
-const expandPrice = document.querySelector('#expand_prices')
-let isPriceExpanded = false
-expandPrice.onclick = () => {
-    if (!isPriceExpanded) {
-        document.querySelector('.prices').style.height = 'unset'
-        isPriceExpanded = true
-    } else {
-        document.querySelector('.prices').style.height = '30px'
-        isPriceExpanded = false
-    }
-}
-
-const closeFilter = document.querySelector('#close_filters')
-closeFilter.onclick = () => {
-    document.querySelector('.mobile-filter').style.display = 'none'
-}
-
-const openFilter = document.querySelector('#filter_btn')
-openFilter.onclick = () => {
-    document.querySelector('.mobile-filter').style.display = 'block'
-}
-
-const closeOrder = document.querySelector('#close_orders')
-closeOrder.onclick = () => {
-    document.querySelector('.mobile-order').style.display = 'none'
-}
-
-const openOrder = document.querySelector('#order_btn')
-openOrder.onclick = () => {
-    document.querySelector('.mobile-order').style.display = 'block'
-}
+const resetFilters = document.querySelector('#reset');
+resetFilters.onclick = () => location.reload();
